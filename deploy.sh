7a4c4f4e1758f8e33177fdb7d699daaef1a606c0
btxuyenhcmus <btxuyenhcmus@gmail.com>
2022-04-04 16:46:04 +0700

    INIT

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+git checkout deploy
+git reset --hard origin/master
+
+npm install
+npm build
+
+find * -maxdepth 0 -name 'build' -prune -o -exec rm -rf '{}' ';'
+
+mv ./build/* .
+
+git rm -rf --cache .
+git add .
+git commit -m "Deploy"
+
+git push origin deploy --force
+
+git checkout master
